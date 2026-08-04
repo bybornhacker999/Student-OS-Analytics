05f8052c4316877c6a8f3c7acce54bb52e59aef0
bybornhacker999 <142158440+bybornhacker999@users.noreply.github.com>
2026-08-04 08:27:35 +0600

    Update data.js

--- data.js
+++ data.js
@@ -1,35 +1,34 @@
-
 const labels = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
 const subjects = [
     {
         name: "Physics",
-        color: "#4FC3F7",
+        color: "#2196F3", // Blue
         data: [20, 28, 35, 45, 60, 74, 82]
     },
     {
         name: "Chemistry",
-        color: "#66BB6A",
+        color: "#FF9800", // Orange
         data: [15, 22, 30, 38, 46, 61, 70]
     },
     {
         name: "Higher Math",
-        color: "#AB47BC",
+        color: "#B57EDC", // Lavender
         data: [25, 32, 40, 53, 67, 73, 90]
     },
     {
         name: "Biology",
-        color: "#FF8A65",
+        color: "#FFFFFF", // White
         data: [18, 25, 36, 42, 58, 70, 80]
     },
     {
         name: "Bangla",
-        color: "#EF5350",
+        color: "#1B5E20", // Dark Green
         data: [30, 35, 43, 55, 66, 72, 85]
     },
     {
         name: "English",
-        color: "#FFD54F",
+        color: "#F44336", // Red
         data: [22, 30, 38, 46, 59, 69, 77]
     }
 ];
